b17a46cc046570b6688e5e5a539c906d57e2d730
aowuor <angelaowuor@gmail.com>
2022-12-06 01:22:56 +0300

    commit tree f5837d1fcd9c17b51c775a768761cc3778412f5b

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+function submitData(nameValue, emailValue){
+    
+    const configurationObj = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: nameValue,
+            email: emailValue,
+        })
+    }
+
+    fetch("http://localhost:3000/users", configurationObj)
+    .then(response => response.json())
+    .then((data) => {
+        let p = document.createElement('p');
+        p.innerHTML = data.id;
+        document.body.appendChild(p)
+        console.log(body)
+    })
+    .catch(error => {
+        const errorMsg = document.createElement('p')
+        errorMsg.innerHTML = error.message;
+        document.body.appendChild(errorMsg)
+    
+    })
+}
+
+submitData("Steve","steve@steve.com")
